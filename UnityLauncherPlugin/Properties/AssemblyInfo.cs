54592cf859be856aefb822f43b39315749867f68
David Lechner <david@lechnology.com>
2016-04-18 17:32:44 -0500

    bump some versions

--- UnityLauncherPlugin/Properties/AssemblyInfo.cs
+++ UnityLauncherPlugin/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Lechner")]
 [assembly: AssemblyProduct("KeePass Plugin")]
-[assembly: AssemblyCopyright("2014 David Lechner")]
+[assembly: AssemblyCopyright("2014-2016 David Lechner")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
